1062c2e6b51d6832a504ede806b70d4397608cee
Yamparala Rahul <rahulvignanwork@gmail.com>
2026-02-18 15:41:05 +0530

    chore: restrict AgentationProvider to dev environment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
             <main className="flex-1">{children}</main>
             <Footer />
           </div>
-          <AgentationProvider />
+          {process.env.NODE_ENV === "development" && <AgentationProvider />}
         </ThemeProvider>
       </body>
     </html>
